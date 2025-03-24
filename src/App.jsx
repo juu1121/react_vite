c64c90c54225e2b57c524e47bad03f6ef7ffb777
Jang hee ju <s6000000@naver.com>
2025-03-24 17:57:28 +0900

    250324_수업끝

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,9 @@ function App(){
                 //여기가 실행되면 사용가능한 token 이라는 의미이다 
                 //토큰을 디코딩해서 userName 을 얻어온다. 
                 const decoded=jwtDecode(token.substring(7));
-                const exp = decoded.exp * 1000; 
+                const exp = decoded.exp * 1000; // 초 → 밀리초
                 const now = Date.now();
-
+                //토큰의 남은 유효시간 얻어내기
                 const remainingTime = exp - now;
                 //자동 로그아웃 예약
                 const logoutTimer=setTimeout(()=>{
@@ -88,16 +88,37 @@ function App(){
                 <AnimatePresence mode="wait">
                     {/* key가 바뀌면 AnimatePresence가 페이지 전환으로 인식 */}
                     <motion.div
-                        key={location.pathname}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.2 }}
+                        key={location.pathname} //경로변경감지
+                        initial={{ opacity: 0 }}    //초기상태
+                        animate={{ opacity: 1 }}    //애니메이셕효과
+                        exit={{ opacity: 0 }}   //사라질떄효과과
+                        transition={{ duration: 0.2 }} //적용되는시간
                     >
                        <div>{currentOutlet}</div>
                     </motion.div>
                 </AnimatePresence>
             </div>
+
+            {/* key가 바뀌면 AnimatePresence가 페이지 전환으로 인식 */}
+            {/* <div style={{ position: "relative", overflow: "hidden", height: "100vh"  }}>
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={location.pathname}
+                        initial={{ x: "100%", opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        exit={{ x: "-100%", opacity: 0 }}
+                        transition={{ duration: 0.4, ease: "easeInOut" }}
+                        style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                        }}
+                    >
+                        <div>{currentOutlet}</div>
+                    </motion.div>
+                </AnimatePresence>
+            </div> */}
+
             </div>
             <LoginModal show={loginModal.show}/>
         </>
